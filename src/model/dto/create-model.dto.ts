87458bff2c6257b47357bf91361aeafc9be24c0d
Ildefonso Valenzuela <ponchovalo@outlook.com>
2024-05-03 11:23:45 -0600

    inicioMay

--- src/model/dto/create-model.dto.ts
+++ src/model/dto/create-model.dto.ts
@@ -1,1 +1,24 @@
-export class CreateModelDto {}
+import { IsArray, IsString } from "class-validator";
+import { CountOid } from "../interfaces/count-oid.interface";
+import { LevelOid } from "../interfaces/level-oid.interface";
+
+export class CreateModelDto {
+
+    @IsString()
+    brand: string;
+
+    @IsString()
+    name: string;
+
+    @IsString()
+    type: string;
+
+    @IsArray()
+    countOids: CountOid[];
+
+    @IsArray()
+    levelOids: LevelOid[];
+
+
+
+}
