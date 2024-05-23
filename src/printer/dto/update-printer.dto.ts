a0466ba0b71c95353323689b5c61c54dbe873188
Ildefonso Valenzuela <ponchovalo@outlook.com>
2024-05-23 17:52:33 -0600

    resolve edit oid printer

--- src/printer/dto/update-printer.dto.ts
+++ src/printer/dto/update-printer.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePrinterDto } from './create-printer.dto';
+import { Oid } from 'src/printer-model/interfaces/oid.interface';
+import { IsArray, IsOptional } from 'class-validator';
 
-export class UpdatePrinterDto extends PartialType(CreatePrinterDto) {}
+export class UpdatePrinterDto extends PartialType(CreatePrinterDto) {
+
+    @IsArray()
+    @IsOptional()
+    printerCountOids?: Oid[];
+
+    @IsArray()
+    @IsOptional()
+    printerLevelOids?: Oid[];
+
+
+}
